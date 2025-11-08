4d7fccd72492d2793c2344cd9accb098535f6d94
Aaron-Rojas <Arojasolano@gmail.com>
2025-11-08 18:51:37 -0500

    Pruebas en IAM/ValesObject/ Email y PasswordHash

--- src/main/java/com/irentaspro/iam/domain/model/valueobject/PasswordHash.java
+++ src/main/java/com/irentaspro/iam/domain/model/valueobject/PasswordHash.java
@@ -1,13 +1,31 @@
 package com.irentaspro.iam.domain.model.valueobject;
 
+import org.aspectj.weaver.ast.Test;
+
 public class PasswordHash {
     private final String valor;
     private final String algoritmo;
 
     public PasswordHash(String valor, String algoritmo) {
+        
+    //Codígo adicional para que pase el test
+    //Test 1
+    if (valor == null ) {
+        throw new IllegalArgumentException("El valor del hash no puede ser nulo");
+    }
+    //Test 2
+    if (algoritmo == null ) {
+        throw new IllegalArgumentException("El algoritmo no puede ser nulo");
+    }
+    //Test 3
+    if (valor.isEmpty() ) {
+        throw new IllegalArgumentException("El valor del hash no puede estar vacío");
+    }
+    
         this.valor = valor;
         this.algoritmo = algoritmo;
     }
+    
 
     public String getValor() {
         return valor;
@@ -16,4 +34,5 @@ public class PasswordHash {
     public String getAlgoritmo() {
         return algoritmo;
     }
+
 }
---
+++ src/test/java/com/irentaspro/iam/domain/model/valueobject/EmailTest.java
@@ -0,0 +1,53 @@
+package com.irentaspro.iam.domain.model.valueobject;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+    //Test Forzando Excepción
+    @Test
+        void EmailInvalido() {
+            // 1. Inicializar 
+            String valorEmail = "correo-invalido.com"; // Sin @
+
+            // 2. Accción de la excepción 
+            assertThrows(IllegalArgumentException.class, () -> {
+                // Verificar
+                new Email(valorEmail);
+            });
+        }  
+
+        
+    @Test
+    void EmailNulo(){
+        
+        String valorEmail = null;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Email(valorEmail);
+        });
+    }
+
+    @Test
+    void EmailVacio(){
+
+        String valorEmail = "";
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Email(valorEmail);
+        });
+    }
+
+    //Test Normales
+    @Test
+    void EmailValido() {
+        //Coloca un email valido 
+        String valorEmail = "Irentaaaa98@gmail.com";
+        //Crea el objeto con el email valido
+        Email email = new Email(valorEmail);
+        //Verifica que el email sea igual al valor proporcionado
+        assertEquals(valorEmail, email.getValor());
+
+    }  
+
+}
