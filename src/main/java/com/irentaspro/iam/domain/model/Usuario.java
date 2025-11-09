6836a842ba27b14d3c26cbdf62e365965633ee7d
Aaron-Rojas <Arojasolano@gmail.com>
2025-11-08 23:24:18 -0500

    Seguimos en IAM, realización de test de Usuario

--- src/main/java/com/irentaspro/iam/domain/model/Usuario.java
+++ src/main/java/com/irentaspro/iam/domain/model/Usuario.java
@@ -23,21 +23,44 @@ public class Usuario extends AggregateRoot {
     // Métodos de dominio y lógica de negocio aquí
     public boolean autenticar(String credenciales) {
         // Lógica de autenticación
+       
+        //Aplicando TDD para el método -> validarCredencialesIncorrectas
+        if (credenciales == null ) {
+            return false;
+        }
+        if (credenciales.isEmpty()) {
+            return false;
+        }
+
         return true; // Ejemplo simplificado
     }
 
-    public void cambiarPassword(String nuevaPassword) {
+
+    //Aplicando TDD para el test -> cambiarPassword
+    public void cambiarPassword(PasswordHash nuevaPassword) {
         // Lógica para cambiar la contraseña
+
+        //momentandeo simple para justificar TDD
+        this.passwordHash = nuevaPassword;
+        if(nuevaPassword == null) {
+            throw new IllegalArgumentException("La nueva contraseña no puede ser nula.");
+        }
+        
     }
 
     public boolean esPremium() {
-        return "premiun".equalsIgnoreCase(this.tipoCuenta);
+        //Aplicando TDD, correción ortografía
+        // para justificar el tdd xd
+        return "premium".equalsIgnoreCase(this.tipoCuenta);
     }
 
+    //Modificación por TDD  Test -> generarToken
     public String generarToken() {
-        return null;
         // Lógica pendiente
+        //momentandeo simple para justificar TDD, esto se debe usar con UUID
+        return UUID.randomUUID().toString();
     }
+        
 
     @Override
     public void validarInvariantes() {
